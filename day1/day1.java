755d852192effcaa7bda88ee1f40c1f0879b1bce
onemarshmallow <yukiyu859@gmail.com>
2023-06-11 16:33:42 -0400

    first commit

---
+++ day1/day1.java
@@ -0,0 +1,9 @@
+package day1;
+
+class day1 {
+    public static void main(String[] args) {
+        System.out.println("-----------------");
+        System.out.println("hiiii");
+        System.out.println("-----------------");
+    }
+}
