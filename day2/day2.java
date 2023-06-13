f92c3087e7ce2b7991690c79a83599ec3453ad61
onemarshmallow <yukiyu859@gmail.com>
2023-06-13 17:51:51 -0400

    finished up part 2

--- day2/day2.java
+++ day2/day2.java
@@ -1,5 +1,7 @@
-// first parse the two values from input
-// then check if i win and calculate score
+// Part 1 and 2 Complete!
+// https://adventofcode.com/2022/day/2
+// This parses two chars from a line in a text file and calculates a score based on the chars.
+
 package day2;
 
 import java.io.File;
@@ -10,10 +12,14 @@ public class day2 {
     public static void main(String[] args) {
         System.out.println("---------------");
         part1();
+        System.out.println();
+        part2();
         System.out.println("---------------");
     }
     
     private static void part1() {
+        // prints the score of each round from given opponent moves and my moves
+        System.out.println("Part 1");
         try {
             File input = new File("day2\\input.txt");
             Scanner myReader = new Scanner(input);
@@ -41,11 +47,43 @@ public class day2 {
         }
     }
 
+    private static void part2() {
+        // print the score from the input of opponent moves and intended result of each round
+        System.out.println("Part 2");
+        try {
+            File input = new File("day2\\input.txt");
+            Scanner myReader = new Scanner(input);
+            int score = 0;
+            int totalScore = 0;
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] moves = data.split(" ");
+                char oppMove = moves[0].charAt(0); // the opponent's move; A for Rock, B for Paper, or C for Scissors
+                char result = moves[1].charAt(0); // X for lose, Y for tie, Z for win
+
+                char myMove = calcMyMove(oppMove, result);
+                score = calcScore(oppMove, myMove);
+                totalScore += score;
+                // System.out.println("Opponent's move: " + oppMove);
+                // System.out.println("My move: " + myMove);
+                // System.out.println("Score for this round is " + score);
+                // System.out.println();
+
+            }
+            System.out.println("Total score is " + totalScore);
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+    }
+
     private static int calcScore(char oppMove, char myMove) {
         int score = 0;
         if (myMove == 'X') { // rock
             score += 1; // as per prompt, 1 point for throwing rock
-            
+
             if (oppMove == 'A') { // rock vs rock, you tie
                 score += 3;
             } else if (oppMove == 'C') { // scissors vs rock, you win
@@ -70,5 +108,38 @@ public class day2 {
         }
 
         return score;
+    }
+
+    private static char calcMyMove(char oppMove, char result) {
+        if (result == 'Y') { // tie
+            // plays the same move
+            if (oppMove == 'A') {
+                return 'X';
+            } else if (oppMove == 'B') {
+                return 'Y';
+            } else if (oppMove == 'C') {
+                return 'Z';
+            }
+        } 
+        else if (result == 'X') { // lose
+            if (oppMove == 'A') {
+                return 'Z'; // to lose to rock, play scissors
+            } else if (oppMove == 'B') {
+                return 'X'; // to lose to paper, play rock
+            } else if (oppMove == 'C') {
+                return 'Y'; // to lose to scissors, play paper
+            }
+        }
+        else if (result == 'Z') { // win
+            if (oppMove == 'A') {
+                return 'Y'; // to win against rock, play paper
+            } else if (oppMove == 'B') {
+                return 'Z'; // to win against paper, play scissors
+            } else if (oppMove == 'C') {
+                return 'X'; // to win against scissors, play rock
+            }
+        }
+        
+        return ' '; // a placeholder, because the if statement above will trigger a separate return.
     }
 }
