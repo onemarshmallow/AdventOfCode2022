945eb925143d26ecbbb1c3d33961958f60a2990c
onemarshmallow <yukiyu859@gmail.com>
2023-06-17 17:16:39 -0400

    finished part 2 in day4 and added comments

--- day4/day4.java
+++ day4/day4.java
@@ -1,3 +1,9 @@
+// This breaks apart a line in a text file into two pairs of ints which are used to mark the boundaries
+// of imaginary "assignments" of sections. These bounds are checked for whether the assignments 
+// completely contain each other (part 1) or whether the assignments overlap (part 2).
+// Part 1 and 2 Complete!
+// https://adventofcode.com/2022/day/4
+
 package day4;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,13 +12,15 @@ import java.util.Scanner;
 public class day4 {
     public static void main(String[] args) {
         System.out.println("-------------");
-        part1();
+        // part1();
+        part2();
         // System.out.println(countContained(3, 96, 3, 97));
         System.out.println();
         System.out.println("-------------");
     }
 
     private static void part1() {
+        // counts completely contained pairs
         System.out.println("Part 1");
         try {
             File input = new File("day4\\input.txt");
@@ -50,6 +58,48 @@ public class day4 {
         }
     }
 
+    private static void part2() {
+        // counts overlapping assignment pairs
+        System.out.println("Part 2");
+        try {
+            File input = new File("day4\\input.txt");
+            Scanner myReader = new Scanner(input);
+            int overlappingPairsCount = 0;
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                // special note about split: https://stackoverflow.com/questions/3481828/how-do-i-split-a-string-in-java
+                // splits line based on the comma 
+                String[] pair = data.split(","); // example: ["2-4", "6-8"]
+                // removes hyphen and separates the two numbers
+                String[] section1 = pair[0].split("-"); // example: ["2", "4"]
+                String[] section2 = pair[1].split("-"); // example: ["6", "8"]
+                // these two below vars are used to store the String[] section1 and section2 as int[]
+                int[] sect1 = new int[2];
+                int[] sect2 = new int[2];
+                for (int i = 0; i < section1.length; i++) {
+                    // turns string[] into int[]
+                    sect1[i] = Integer.valueOf(section1[i]);
+                    sect2[i] = Integer.valueOf(section2[i]);
+                }
+
+                overlappingPairsCount += countOverlapping(sect1[0], sect1[1], sect2[0], sect2[1]);
+                // System.out.println("section1: " + section1[0] + ", " + section1[1]);
+                // System.out.println("section2: " + section2[0] + ", " + section2[1]);
+                // System.out.println("sect1: " + sect1[0] + ", " + sect1[1]);
+                // System.out.println("sect2: " + sect2[0] + ", " + sect2[1]);
+                // System.out.println();
+            }
+
+            System.out.println("There are " + overlappingPairsCount + " overlapping assignment pairs.");
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error has occured.");
+            e.printStackTrace();
+        }
+    }
+
     private static int countContained(int sect1_lowerBound, int sect1_upperBound, int sect2_lowerBound,
             int sect2_upperBound) {
         int counter = 0;
@@ -64,5 +114,15 @@ public class day4 {
         return counter;
     }
 
-    private static int countOverlapping(int sect1_lb, int sect1_ub, int)
+    private static int countOverlapping(int sect1_lb, int sect1_ub, int sect2_lb, int sect2_ub) {
+        // note for future: this function would work better with returning a boolean instead of int.
+        // lb stands for lower bound; ub stands for upper bound
+
+        int counter = 0;
+        if ((sect1_ub - sect2_lb >= 0) && (sect2_ub - sect1_lb >= 0)) {
+            counter++;
+        }
+        return counter;
+    }
+
 }
